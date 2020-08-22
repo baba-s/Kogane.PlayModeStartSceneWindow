2785fc96d7bcc2542ed20378178686b3ec1043fc
baba_s <shotababa1206@gmail.com>
2020-08-22 15:27:18 +0900

    first commit

---
+++ Editor/PlayModeStartSceneSettingWindow.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Kogane.Internal
+{
+	internal sealed class PlayModeStartSceneSettingWindow : EditorWindow
+	{
+		[MenuItem( "Window/UniPlayModeStartSceneSettingWindow" )]
+		private static void Open()
+		{
+			var title   = "UniPlayModeStartSceneSettingWindow";
+			var window  = GetWindow<PlayModeStartSceneSettingWindow>( title );
+			var minSize = window.minSize;
+
+			minSize.y      = 40;
+			window.minSize = minSize;
+		}
+
+		private void OnGUI()
+		{
+			EditorSceneManager.playModeStartScene = ( SceneAsset ) EditorGUILayout.ObjectField
+			(
+				label: new GUIContent( "Start Scene" ),
+				obj: EditorSceneManager.playModeStartScene,
+				objType: typeof( SceneAsset ),
+				allowSceneObjects: false
+			);
+
+			var activeScene = SceneManager.GetActiveScene();
+			var path        = activeScene.path;
+			var filename    = Path.GetFileNameWithoutExtension( path );
+			var isValid     = !string.IsNullOrWhiteSpace( path );
+
+			using ( new EditorGUI.DisabledScope( !isValid ) )
+			{
+				if ( GUILayout.Button( "Set Current Scene: " + filename ) )
+				{
+					var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>( path );
+					EditorSceneManager.playModeStartScene = sceneAsset;
+				}
+			}
+		}
+	}
+}
