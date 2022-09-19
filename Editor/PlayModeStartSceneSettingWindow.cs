5eadaadc5977ac9721c902fc091dc0f4ea4fa230
baba-s <shotababa1206@gmail.com>
2022-09-19 14:26:23 +0900

    update

--- Editor/PlayModeStartSceneSettingWindow.cs
+++ Editor/PlayModeStartSceneSettingWindow.cs
@@ -6,42 +6,53 @@ using UnityEngine.SceneManagement;
 
 namespace Kogane.Internal
 {
-	internal sealed class PlayModeStartSceneSettingWindow : EditorWindow
-	{
-		[MenuItem( "Window/UniPlayModeStartSceneSettingWindow" )]
-		private static void Open()
-		{
-			var title   = "UniPlayModeStartSceneSettingWindow";
-			var window  = GetWindow<PlayModeStartSceneSettingWindow>( title );
-			var minSize = window.minSize;
+    internal sealed class PlayModeStartSceneSettingWindow : EditorWindow
+    {
+        [MenuItem( "Window/Kogane/Play Mode Start Scene", false, 1541845126 )]
+        private static void Open()
+        {
+            const string title = "Play Mode Start Scene";
 
-			minSize.y      = 40;
-			window.minSize = minSize;
-		}
+            var window = GetWindow<PlayModeStartSceneSettingWindow>( title );
 
-		private void OnGUI()
-		{
-			EditorSceneManager.playModeStartScene = ( SceneAsset ) EditorGUILayout.ObjectField
-			(
-				label: new GUIContent( "Start Scene" ),
-				obj: EditorSceneManager.playModeStartScene,
-				objType: typeof( SceneAsset ),
-				allowSceneObjects: false
-			);
+            const int height = 40;
 
-			var activeScene = SceneManager.GetActiveScene();
-			var path        = activeScene.path;
-			var filename    = Path.GetFileNameWithoutExtension( path );
-			var isValid     = !string.IsNullOrWhiteSpace( path );
+            var position = window.position;
+            position.height = height;
+            window.position = position;
 
-			using ( new EditorGUI.DisabledScope( !isValid ) )
-			{
-				if ( GUILayout.Button( "Set Current Scene: " + filename ) )
-				{
-					var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>( path );
-					EditorSceneManager.playModeStartScene = sceneAsset;
-				}
-			}
-		}
-	}
+            var minSize = window.minSize;
+            minSize.y      = height;
+            window.minSize = minSize;
+
+            var maxSize = window.maxSize;
+            maxSize.y      = height;
+            window.maxSize = maxSize;
+        }
+
+        private void OnGUI()
+        {
+            EditorSceneManager.playModeStartScene = ( SceneAsset )EditorGUILayout.ObjectField
+            (
+                label: new GUIContent( "Start Scene" ),
+                obj: EditorSceneManager.playModeStartScene,
+                objType: typeof( SceneAsset ),
+                allowSceneObjects: false
+            );
+
+            var activeScene = SceneManager.GetActiveScene();
+            var path        = activeScene.path;
+            var filename    = Path.GetFileNameWithoutExtension( path );
+            var isValid     = !string.IsNullOrWhiteSpace( path );
+
+            using ( new EditorGUI.DisabledScope( !isValid ) )
+            {
+                if ( GUILayout.Button( "Set Current Scene: " + filename ) )
+                {
+                    var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>( path );
+                    EditorSceneManager.playModeStartScene = sceneAsset;
+                }
+            }
+        }
+    }
 }
